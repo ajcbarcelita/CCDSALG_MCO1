3071e3cd2c891e6bb4b2c28cc5deff035e11e9e9
Aaron Barcelita <aaron_barcelita@dlsu.edu.ph>
2024-05-15 14:19:45 +0800

    Added main function and selection sort.

--- main.c
+++ main.c
@@ -4,12 +4,82 @@
 #include "timer.c"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main()
 {
-    
+    int status, choice;
+    int recordCount; //number of records in file
+    long startTime, endTime, executionTime;
+    pathName path; //path of .txt file to be sorted
 
+    do 
+    {
+        printf("\t=== CHOOSING A FILE ===\n\n");
+        printf("Enter file path of .txt file to sort:\n");
+        fgets(path, 500, stdin);
+        path[strcspn(path, "\n")] = '\0';
 
+        status = isFileValid(path);
+        if (status == 0) {
+            printf("File not found. Please try again.\n");
+        } else {
+            printf("File found.\n");
+        }
+        printf("\n");
+	} while (status == 0);
+
+    //do dynamic memory allocation here
+    recordCount = getRecordCount(path);
+    printf("Number of records: %d\n", recordCount);
+	Record *records = (Record *)malloc(recordCount * sizeof(Record));
+
+    //file reading and storing to *records
+    readFile(records, path);
+
+    do
+    {
+        printf("\t=== SORTING ALGORITHMS ==\n\n");
+        printf("Choose a sorting algorithm to use: \n");
+        printf("[1] Insertion Sort.\n");
+        printf("[2] Selection Sort.\n");
+        printf("[3] Merge Sort.\n");
+        printf("[4] Quick Sort.\n");
+        printf("Input: ");
+        scanf("%d", &choice);
+
+        //call timer.c before starting, and after ending algorithm
+        switch (choice)
+        {
+            case 1: //insertion
+
+            break;
+
+            case 2: //selection sort
+                startTime = currentTimeMillis();
+                selectionSort(records, recordCount); //call selection sort here (sortingalgorithms.c
+                endTime = currentTimeMillis();
+                executionTime = endTime - startTime;
+                // printRecordsToFile(records, recordCount); //to double check if the array of structs is sorted
+                printf("Start Time: %ld ms\n End Time: %ld ms\n", startTime, endTime);
+                printf("Time taken: %ld milliseconds.\n", executionTime);
+            break;
+
+            case 3: //merge sort
+
+            break;
+
+            case 4: //quick sort
+
+            break;
+
+            default:
+                printf("Invalid choice. Please try again.\n");
+            break;
+        }
+    } while( choice < 1 || choice > 4);
+
+    free(records);
 
     return 0;
 }
--- sortingalgorithms.c
+++ sortingalgorithms.c
@@ -4,15 +4,76 @@
 #include <stdio.h>
 #include "record.c"
 
+typedef char pathName[500]; //declaration of a type for the path name of the file
+
 /*
-* You may declare additional variables and helper functions
-* as needed by the sorting algorithms here.
+    This function asks for the file path of the .txt file that contains the records.
+    Returns 1 is file is found, 0 if the file is not found. 
 */
+int isFileValid (pathName path)
+{
+    FILE *fp;
+    fp = fopen(path, "r");
+    if (fp == NULL) {
+        printf("Error opening file.\n");
+        return 0; //file is not found
+    } else {
+        return 1; //file found
+    }
+    fclose(fp);
+}
+/*
+    This functions reads the number of records in the .txt file. Assumes that .txt file is already in the correct format,
+    with the first line being the number of records. Returns the number of records in the file.
+*/
+int getRecordCount(pathName path)
+{
+    int recordCount;
+    FILE *fp;
 
+    fp = fopen(path, "r");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        return -1;
+    } else {
+            fscanf(fp, "%d", &recordCount);
+    }
+    fclose(fp);
 
+    return recordCount;
+}
 
+/*
+    This function prints out the contents of the array of structs/records into a .txt file called output.txt
+    Helps ensure that the sorting algorithms are working correctly. For testing purposes.
+*/
+void printRecordsToFile(Record *records, int recordCount) {
+    FILE *file = fopen("output.txt", "w"); //opens file in write mode, if output.txt does not exist, it will be created
+    if (file == NULL) {
+        printf("Error opening file!\n"); //accounts for other instances where file may not be opened
+        return;
+    }
 
+    for(int i = 0; i < recordCount; i++) {
+        fprintf(file, "ID number %d, %s\n", records[i].idNumber, records[i].name);
+    }
 
+    fclose(file);
+}
+
+/*
+    This function allows for the swapping of different elements in an array of structs/records.
+*/
+void swapStructs (Record *a, Record *b)
+{
+    Record temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+/*
+    pseudo-code for insertion sort is as follows
+*/
 
 void insertionSort(Record *arr, int n)
 {
@@ -25,17 +86,31 @@ void insertionSort(Record *arr, int n)
 
 }
 
+
+/*
+    pseudo-code for selection sort is as follows
+*/
 void selectionSort(Record *arr, int n)
 {
     // TODO: Implement this sorting algorithm here.
+    int i, j, minIndex;
 
-
-
-
-
-
+    for (i = 0; i < n - 1; i++) {
+        minIndex = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j].idNumber < arr[minIndex].idNumber) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            swapStructs(&arr[minIndex], &arr[i]);
+        }
+    }
 }
 
+/*
+    pseudo-code for merge sort is as follows
+*/
 void mergeSort(Record *arr, int p, int r)
 {
     // TODO: Implement this sorting algorithm here.
@@ -53,14 +128,13 @@ void mergeSort(Record *arr, int p, int r)
 * record structures.
 */
 
+/*
+    pseudo-code for quick sort is as follows
+*/
+void quickSort()
+{
 
-
-
-
-
-
-
-
+}
 
 
 #endif
